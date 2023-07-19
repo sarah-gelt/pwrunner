9f4af43bb85cff7aa6f55beea53025f2b3a9d67a
Sarah Woodhouse <sarah@joingelt.com>
2023-07-19 17:02:58 +0100

    Add test suites

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,22 @@
 import { chromium, expect, test } from '@playwright/test';
 
-test('Load docs', async ({ page }) => {
-  await page.goto("https://playwright.dev/docs/intro");
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test.describe("Documentation", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://playwright.dev/docs/intro");
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
 
-test.afterAll(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto("https://playwright.dev");
-});
+  test.describe("Getting Started", () => {
+    test('Load installation page', async ({ page }) => {
+      await page.getByRole('link', { name: 'Writing tests', exact: true }).click();
+      await expect(page.getByRole('heading', { name: 'Writing tests' })).toBeVisible();
+    });
+
+    test.afterAll(async () => {
+      const browser = await chromium.launch();
+      const page = await browser.newPage();
+      await page.goto("https://playwright.dev");
+    });
+  });
+})
 
